ce7b74196c3507e324c877fd820b2c56f7e5f951
BalrajMahto <balrajkvbndm@gmail.com>
2026-07-28 15:03:45 +0530

    added cors

--- server.js
+++ server.js
@@ -1,30 +1,28 @@
 import express from "express";
-
 import dotenv from "dotenv";
-
 import cors from "cors";
 
 import connectDB from "./config/database.js";
-
 import wishRoutes from "./routes/wish.routes.js";
 
 dotenv.config();
-console.log(process.env.MONGODB_URI);
 
 connectDB();
 
-const app=express();
+const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN
+  })
+);
 
 app.use(express.json());
 
-app.use("/api/wishes",wishRoutes);
+app.use("/api/wishes", wishRoutes);
 
-const PORT=process.env.PORT||5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-
-console.log(`Server Running on ${PORT}`);
-
+app.listen(PORT, () => {
+    console.log(`Server Running on ${PORT}`);
 });
